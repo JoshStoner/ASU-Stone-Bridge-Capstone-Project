dedecb888d4ebb0dbbf0b29444b8986f315bb2c1
dwallen3 <56324889+dwallen3@users.noreply.github.com>
2021-04-16 15:52:47 -0700

    needs library csv imported

--- StoneBridge Well Inspection W: Database/StoneBride Well Inspection/StoneBride Well Inspection/Models/Database.swift
+++ StoneBridge Well Inspection W: Database/StoneBride Well Inspection/StoneBride Well Inspection/Models/Database.swift
@@ -483,9 +483,26 @@ class Database
         inspectionTitle = wName + wIDate + ".csv" // this is for the file name
         
         
+        let docsPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as String
+        
+        let docsURL = URL(fileURLWithPath: docsPath).appendingPathComponent(inspectionTitle)
+        
+        let output = OutputStream.toMemory()
+        
+        let csvw = CHCSVWriter(outputStream: output, encoding: String.Encoding.utf8.rawValue, delimiter: ",".utf16.first!)
+        
+        
+        
         let path = NSURL(fileURLWithPath: NSHomeDirectory()).appendingPathComponent(inspectionTitle) //i think the home directory hear adds it to docs
         
         //let sqlCmd = "SELECT * FROM tablename ORDER BY column DESC LIMIT 1;" //this is a query for selecting the most recent entry, not sure how to implement
+        
+        
+        csvw?.writeField("Employee Name")
+        csvw?.writeField("Employee ID")
+        
+        csvw?.finishLine()
+        
         
         
         var csvTxt = "Employee Name,Employee ID\n"
@@ -500,7 +517,25 @@ class Database
             let eLine = "\(employee[self.Name]),\(employee[self.employeeID])\n"
             csvTxt.append(eLine)
             
+            
+            csvw?.writeField("\(employee[self.Name])")
+            csvw?.writeField("\(employee[self.employeeID])")
+            
+            csvw?.finishLine()
+            
         }
+        
+        
+        csvw?.finishLine()
+        
+        
+        
+        
+        csvw?.writeField("Employee ID")
+        csvw?.writeField("Well ID")
+        csvw?.writeField("Date")
+        
+        csvw?.finishLine()
         
         let fTable = "Employee ID,Well ID,Date\n"
         
@@ -516,7 +551,27 @@ class Database
             
             let fLine = "\(fill[self.employeeID]),\(fill[self.wellID]),\(fill[self.date])\n"
             csvTxt.append(fLine)
+            
+            csvw?.writeField("\(fill[self.employeeID])")
+            csvw?.writeField("\(fill[self.wellID])")
+            csvw?.writeField("\(fill[self.date])")
+            
+            csvw?.finishLine()
+            
+            
+            
+            
+            
         }
+        
+        
+        csvw?.finishLine()
+        
+        csvw?.writeField("Well ID")
+        csvw?.writeField("Well Name")
+        csvw?.writeField("Date")
+        
+        csvw?.finishLine()
         
         let iFTable = "Well ID,Well Name,Date\n"
         csvTxt.append(iFTable)
@@ -533,7 +588,21 @@ class Database
             let iFLine = "\(Inspection[self.wellID]),\(Inspection[self.wellName]),\(Inspection[self.date])\n"
             csvTxt.append(iFLine)
             
+            csvw?.writeField("\(Inspection[self.wellID])")
+            csvw?.writeField("\(Inspection[self.wellName])")
+            csvw?.writeField("\(Inspection[self.date])")
+            
+            csvw?.finishLine()
+            
         }
+        
+        csvw?.finishLine()
+        
+        csvw?.writeField("Well ID")
+        csvw?.writeField("Date")
+        csvw?.writeField("Pic ID")
+        
+        csvw?.finishLine()
         
         let wITable = "Well ID,Date,PicID\n"
         csvTxt.append(wITable)
@@ -548,7 +617,20 @@ class Database
             
             let wILine = "\(wellImage[self.wellID]),\(wellImage[self.date]),\(wellImage[self.PicID])\n"
             csvTxt.append(wILine)
+            
+            csvw?.writeField("\(wellImage[self.wellID])")
+            csvw?.writeField("\(wellImage[self.date])")
+            csvw?.writeField("\(wellImage[self.PicID])")
+            
+            csvw?.finishLine()
         }
+        
+        csvw?.finishLine
+        
+        csvw?.writeField("Pic ID")
+        csvw?.writeField("Char ID")
+        
+        csvw?.finishLine()
         
         let pTable = "Pic ID,Char ID\n"
         csvTxt.append(pTable)
@@ -568,7 +650,19 @@ class Database
             let pLine = "\(Picture[self.PicID]),\(Picture[self.charID])\n"
             csvTxt.append(pLine)
             
+            csvw?.writeField("\(Picture[self.PicID])")
+            csvw?.writeField("\(Picture[self.charID])")
+            
+            csvw?.finishLine()
         }
+        
+        csvw?.finishLine()
+        
+        csvw?.writeField("Well ID")
+        csvw?.writeField("Date")
+        csvw?.writeField("Char ID")
+        
+        csvw?.finishLine()
         
         let wDTable = "well ID,Date,Char ID\n"
         csvTxt.append(wDTable)
@@ -583,8 +677,23 @@ class Database
             
             let wDLine = "\(wellDesc[self.wellID]),\(wellDesc[self.date]),\(wellDesc[self.charID])\n"
             csvTxt.append(wDLine)
+            
+            csvw?.writeField("\(wellDesc[self.wellID])")
+            csvw?.writeField("\(wellDesc[self.date])")
+            csvw?.writeField("\(wellDesc[self.charID])")
+            
+            csvw?.finishLine()
         }
         
+        csvw?.finishLine()
+        
+        csvw?.writeField("Category")
+        csvw?.writeField("Y/N")
+        csvw?.writeField("Comment")
+        csvw?.writeField("Description ID")
+        csvw?.writeField("Well ID")
+        
+        csvw?.finishLine()
         
         let weTable = "Category,Y/N,Comment,Description ID,Well ID\n"
         csvTxt.append(weTable)
@@ -608,19 +717,32 @@ class Database
             
             let weLine = "\(Well[self.Category]),\(Well[self.YesOrNo]),\(Well[self.comment]),\(Well[self.charID]),\(Well[self.wellID])\n"
             csvTxt.append(weLine)
+            
+            csvw?.writeField("\(Well[self.Category])")
+            csvw?.writeField("\(Well[self.YesOrNo])")
+            csvw?.writeField("\(Well[self.comment])")
+            csvw?.writeField("\(Well[self.charID])")
+            csvw?.writeField("\(Well[self.wellID])")
+            
+            csvw?.finishLine()
         }
+        
+        csvw?.closeStream()
+        
+        let buffer = (output.property(forKey: .dataWrittenToMemoryStreamKey) as? Data)!
         
         if(exists == true){
             print("exists")
         }
         else{
             do{
-                try
-                    csvTxt.write(to: path!, atomically: true, encoding: String.Encoding.utf8)
+                //try csvTxt.write(to: path!, atomically: true, encoding: String.Encoding.utf8)
+                try buffer.write(to: docsURL)
             }
                 catch{
                     print("failed to create file")
                 }
+            
             
             
             
